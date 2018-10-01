b1aa2ea3714486ff0ae9bb594b946ba5ce178357
khuchuan <khuchuan@gmail.com>
2018-10-02 06:46:14 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Sidebar';
+import Main from './Main';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
